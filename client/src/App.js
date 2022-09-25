323981855d3e2305444ec7b2abf9ea55286d3df7
anthonyponplex <mayakianthony@yahoo.com>
2022-09-25 15:10:54 +0100

    Client axios config

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,39 @@
-import React, {Fragment} from 'react'
+import React, { Fragment } from "react";
 
 // Import Modules
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 // Import Utils
-import setAuthToken from './utils/setAuthToken'
+import setAuthToken from "./utils/setAuthToken";
 
 // Import Private Route
-import PrivateRoute from './components/routing/PrivateRoute'
+import PrivateRoute from "./components/routing/PrivateRoute";
 
 // Import Layouts
-import Navbar from './components/layouts/Navbar'
-import Alerts from './components/layouts/Alerts'
+import Navbar from "./components/layouts/Navbar";
+import Alerts from "./components/layouts/Alerts";
 
 // Import Static Pages
-import Home from './components/pages/Home'
-import About from './components/pages/About'
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
 
 // Import Components
-import Register from './components/auth/Register'
-import Login from './components/auth/Login'
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 // Import States
-import ContactState from './context/contact/ContactState'
-import AuthState from './context/auth/AuthState'
-import AlertState from './context/alert/AlertState'
+import ContactState from "./context/contact/ContactState";
+import AuthState from "./context/auth/AuthState";
+import AlertState from "./context/alert/AlertState";
 
 // Import CSS
-import './App.css'
+import "./App.css";
 
 if (localStorage.tokem) {
-  setAuthToken(localStorage.token)
+  setAuthToken(localStorage.token);
 }
+
+axios.defaults.baseURL = "https://phonebook-saver-api.herokuapp.com/";
 
 const App = () => {
   return (
@@ -40,14 +42,14 @@ const App = () => {
         <Router>
           <Fragment>
             <Navbar />
-            <div className='container'>
+            <div className="container">
               <AlertState>
                 <Alerts />
                 <Switch>
-                  <PrivateRoute exact path='/' component={Home} />
-                  <Route exact path='/about' component={About} />
-                  <Route exact path='/register' component={Register} />
-                  <Route exact path='/login' component={Login} />
+                  <PrivateRoute exact path="/" component={Home} />
+                  <Route exact path="/about" component={About} />
+                  <Route exact path="/register" component={Register} />
+                  <Route exact path="/login" component={Login} />
                 </Switch>
               </AlertState>
             </div>
@@ -55,7 +57,7 @@ const App = () => {
         </Router>
       </ContactState>
     </AuthState>
-  )
-}
+  );
+};
 
-export default App
+export default App;
